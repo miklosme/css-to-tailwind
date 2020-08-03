8d8109a881d094f23e743c9e5a1c9f41ad6af81c
Miklos Megyes <miklos@eclecticiq.com>
2020-08-04 01:17:16 +0200

    normalize classes

--- index.js
+++ index.js
@@ -2,70 +2,56 @@ const parse = require("postcss-safe-parser");
 const fs = require("fs").promises;
 const { JSDOM } = require("jsdom");
 
-async function tailwindClasses(css) {
-//   const result = await parse(`.foo{border:1px solid red}`);
+async function listTailwindClasses(css) {
   const result = await parse(css);
-
-  const json = {};
+  const classes = [];
 
   result.walkRules((rule) => {
-    let item = null;
-
     if (rule.parent.type === "root" && /^\.[a-z0-9-]+$/.test(rule.selector)) {
-      json[rule.selector] = [];
-      rule.walkDecls((decl) => {
-        json[rule.selector].push([decl.prop, decl.value]);
-      });
+      classes.push(rule.selector.slice(1));
     }
   });
 
-  return json;
+  return classes;
 }
 
-// these are not relevant for us, but they are always there in the computed style
-function noJunkProps(key) {
-  return key !== "display" && key !== "visibility";
+function normalizeTailwindClasses(css, tailwindClasses) {
+  const elements = tailwindClasses
+    .map((tc) => `<div class="${tc}" />`)
+    .join("");
+  const dom = new JSDOM(`<!DOCTYPE html><style>${css}</style>${elements}`);
+
+  return tailwindClasses.slice(0, 10).reduce((acc, tailwindClass) => {
+    console.log(tailwindClass);
+    const main = dom.window.document.querySelector(`.${tailwindClass}`);
+    const computedStyle = dom.window.getComputedStyle(main);
+    const { display, visibility, ...normalized } = computedStyle._values;
+
+    return {
+      ...acc,
+      [tailwindClass]: normalized,
+    };
+  }, {});
 }
 
-function computeClass(touples) {
-  const css = touples.map(([prop, val]) => `${prop}: ${val}`).join(";");
-  const dom = new JSDOM(
-    `<!DOCTYPE html><style>#test{${css}}</style><p id="test">Hello world</p>`
-  );
-  const main = dom.window.document.querySelector("#test");
-  const computedStyle = dom.window.getComputedStyle(main);
-  return Object.fromEntries(
-    Object.entries(computedStyle._values).filter(noJunkProps)
-  );
+async function tailwindJson(css) {
+  const tailwindClasses = await listTailwindClasses(css);
+  return normalizeTailwindClasses(css, tailwindClasses);
 }
 
 (async () => {
   const css = await fs.readFile("./tailwind.css", "utf8");
-  const tailwindJson = await tailwindClasses(css);
-
-  const result = Object.fromEntries(
-    Object.entries(tailwindJson)
-      .map(([tailwindClass, touples]) => {
-        return [tailwindClass, computeClass(touples)];
-      })
-  );
+  const result = await tailwindJson(css);
 
   console.log(result);
   debugger;
 })();
 
-// import * as postcss from 'postcss';
-
-// export default postcss.plugin('postcss-reverse-props', (options = {}) => {
-//     // Work with options here
-//     return root => {
-//         // Transform CSS AST here
-//         root.walkRules(rule => {
-//             // Transform each rule here
-//             rule.walkDecls(decl => {
-//                 // Transform each property declaration here
-//                 decl.prop = decl.prop.split('').reverse().join('');
-//             });
-//         });
-//     };
+// result.walkRules((rule) => {
+//   if (rule.parent.type === "root" && /^\.[a-z0-9-]+$/.test(rule.selector)) {
+//     json[rule.selector] = [];
+//     rule.walkDecls((decl) => {
+//       json[rule.selector].push([decl.prop, decl.value]);
+//     });
+//   }
 // });
