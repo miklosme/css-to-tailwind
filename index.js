53c3f70330f252742a027a2aeb63baf1ac17e268
Miklos Megyes <miklos@eclecticiq.com>
2020-08-04 01:17:52 +0200

    prettier

--- index.js
+++ index.js
@@ -1,50 +1,48 @@
-const parse = require("postcss-safe-parser");
-const fs = require("fs").promises;
-const { JSDOM } = require("jsdom");
+const parse = require('postcss-safe-parser');
+const fs = require('fs').promises;
+const { JSDOM } = require('jsdom');
 
 async function listTailwindClasses(css) {
-  const result = await parse(css);
-  const classes = [];
+    const result = await parse(css);
+    const classes = [];
 
-  result.walkRules((rule) => {
-    if (rule.parent.type === "root" && /^\.[a-z0-9-]+$/.test(rule.selector)) {
-      classes.push(rule.selector.slice(1));
-    }
-  });
+    result.walkRules((rule) => {
+        if (rule.parent.type === 'root' && /^\.[a-z0-9-]+$/.test(rule.selector)) {
+            classes.push(rule.selector.slice(1));
+        }
+    });
 
-  return classes;
+    return classes;
 }
 
 function normalizeTailwindClasses(css, tailwindClasses) {
-  const elements = tailwindClasses
-    .map((tc) => `<div class="${tc}" />`)
-    .join("");
-  const dom = new JSDOM(`<!DOCTYPE html><style>${css}</style>${elements}`);
+    const elements = tailwindClasses.map((tc) => `<div class="${tc}" />`).join('');
+    const dom = new JSDOM(`<!DOCTYPE html><style>${css}</style>${elements}`);
 
-  return tailwindClasses.slice(0, 10).reduce((acc, tailwindClass) => {
-    console.log(tailwindClass);
-    const main = dom.window.document.querySelector(`.${tailwindClass}`);
-    const computedStyle = dom.window.getComputedStyle(main);
-    const { display, visibility, ...normalized } = computedStyle._values;
+    return tailwindClasses.slice(0, 10).reduce((acc, tailwindClass) => {
+        console.log(tailwindClass);
+        const main = dom.window.document.querySelector(`.${tailwindClass}`);
+        const computedStyle = dom.window.getComputedStyle(main);
+        const { display, visibility, ...normalized } = computedStyle._values;
 
-    return {
-      ...acc,
-      [tailwindClass]: normalized,
-    };
-  }, {});
+        return {
+            ...acc,
+            [tailwindClass]: normalized,
+        };
+    }, {});
 }
 
 async function tailwindJson(css) {
-  const tailwindClasses = await listTailwindClasses(css);
-  return normalizeTailwindClasses(css, tailwindClasses);
+    const tailwindClasses = await listTailwindClasses(css);
+    return normalizeTailwindClasses(css, tailwindClasses);
 }
 
 (async () => {
-  const css = await fs.readFile("./tailwind.css", "utf8");
-  const result = await tailwindJson(css);
+    const css = await fs.readFile('./tailwind.css', 'utf8');
+    const result = await tailwindJson(css);
 
-  console.log(result);
-  debugger;
+    console.log(result);
+    debugger;
 })();
 
 // result.walkRules((rule) => {
