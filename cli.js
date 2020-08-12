4d584d44ef0fe89559d35fd6db33cc814041d327
Miklos Megyes <m.miklos05@gmail.com>
2020-08-12 14:41:32 +0200

    fix cli

--- cli.js
+++ cli.js
@@ -2,7 +2,7 @@
 
 process.chdir(__dirname);
 
-const { cssToTailwind } = require('.');
+const cssToTailwind = require('.');
 
 (async () => {
     console.log(JSON.stringify(await cssToTailwind(process.argv[2]), null, 2));
